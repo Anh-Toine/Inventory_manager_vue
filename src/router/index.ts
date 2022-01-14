72d12b0f48b3fd4d96d892612014eafb1bbd2e65
Dratorz <chhunrandy02@gmail.com>
2022-01-14 02:24:24 -0500

    remove head from router

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/suppliers',
     name: 'SuppliersPage',
     component: Suppliers
-<<<<<<< HEAD
   },
   {
     path: '/categories',
@@ -33,8 +32,6 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-=======
->>>>>>> e1555af (Added the Id's for selenium testing)
   }
 ]
 
