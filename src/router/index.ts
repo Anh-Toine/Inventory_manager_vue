f3c844c706cd8228b87ec94d452b7a99456633dd
Anh-Toine <Nick1234567890a@gmail.com>
2022-01-27 09:55:28 -0500

    View orders

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Products from '../views/Products.vue'
 import Suppliers from '../views/Suppliers.vue'
 import Categories from '../views/Categories.vue'
 import Login from '../views/Login.vue'
+import Orders from '../views/Orders.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -14,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/products',
     name: 'ProductsPage',
     component: Products
+  },
+  {
+    path: '/orders',
+    name: 'OrdersPage',
+    component: Orders
   },
   {
     path: '/suppliers',
