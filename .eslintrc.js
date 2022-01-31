1d374f28c09e286e3db113b6bb7ba07639ccd097
Dratorz <chhunrandy02@gmail.com>
2022-01-30 21:25:20 -0500

    Added the ability to select a category when adding or updating a product via a dropdown list.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-tabs': 0,
-    'no-mixed-spaces-and-tabs': 0
+    'no-mixed-spaces-and-tabs': 0,
+    "@typescript-eslint/no-unused-vars": "off"
   }
 }
