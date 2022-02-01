39686a01abcf6b5838678b588d0cf477fede54df
Anh-Toine <Nick1234567890a@gmail.com>
2022-02-01 09:34:27 -0500

    Tidied up the components file for Orders

--- src/components/OrdersComponent.vue
+++ src/components/OrdersComponent.vue
@@ -206,7 +206,7 @@ export default defineComponent({
 
       axios.put('http://localhost:8080/orders/' + this.orderId, updatejson, this.yourConfig)
         .then(res => {
-          const storedIndex = this.categories.map(x => x.orderDate).indexOf(updatedOrder.orderDate)
+          const storedIndex = this.categories.map(x => x.orderId).indexOf(updatedOrder.orderDate)
           this.orders.splice(storedIndex, 1, res.data)
           this.closeForm()
         })
