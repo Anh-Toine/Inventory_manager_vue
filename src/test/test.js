00c7ea7e6fafff8e918d02259ae57b34e724ffd8
Hathenia <Nick1234567890a@gmail.com>
2022-02-09 10:18:12 -0500

    Nope, not there yet.

--- src/test/test.js
+++ src/test/test.js
@@ -2,7 +2,7 @@ const chai = require('chai')
 // let assert = require('assert');
 const expect = chai.expect
 const assert = chai.assert
-const orderspage = require('../components/OrdersComponent.vue')
+const orderspage = require('../views/Orders.vue')
 describe('Orders are visible', () => {
   it('Check to see if the orders are visible ', function(){
     const wrapper = shallowMount(orderspage)
