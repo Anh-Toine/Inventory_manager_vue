ec41dc09d1f3a9699395e870fa97d9d1e8eaa948
Hathenia <Nick1234567890a@gmail.com>
2022-02-09 21:12:19 -0500

    Removed redundant test case

--- tests/unit/products.spec.ts
+++ tests/unit/products.spec.ts
@@ -1,17 +1,6 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
 import Products from '@/components/ProductsComponent.vue'
-import HelloWorld from '@/components/HelloWorld.vue'
-
-describe("HelloWorld.vue", () => {
-     it("renders props.msg when passed", () => {
-       const msg = "new message";
-       const wrapper = shallowMount(HelloWorld, {
-         props: { msg },
-    });
-    expect(wrapper.text()).to.include(msg);
-  });
-});
 
 describe('ProductsComponent.vue', () => {
   it('Products should be displayed properly', () => {
